6aec74b023e75fa49971cacd698e98429bd78150
DESKTOP-E1P73N6\kevin <kevin3571630@gmail.com>
2022-10-22 19:39:28 +0800

    bvc

---
+++ Exec_Number/Q1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//判斷某數是不是質數
+			int AA = 123;
+
+
+
+
+		}
+	}
+}
---
+++ Exec_Number/q4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q4
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//百元買百雞 公雞5元 母雞3元 小雞3隻一元
+
+
+		}
+	}
+}
